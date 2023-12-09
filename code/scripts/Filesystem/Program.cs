0074a7d9bd03612d0d548764f1f8d51385fd09cf
Joaquin Rapela <joacorapela@gmail.com>
2023-12-09 17:17:06 +0000

    Added

---
+++ code/scripts/Filesystem/Program.cs
@@ -0,0 +1,18 @@
+﻿
+static class RxExt
+{
+    public static IObservable<IList<T>> Quiescent<T>(
+        this IObservable<T> src,
+        TimeSpan minimumInactivityPeriod,
+        IScheduler scheduler)
+    {
+        IObservable<int> onoffs =
+            from _ in src
+            from delta in Observable.Return(1, scheduler).Concat(Observable.Return(-1, scheduler).Delay(minimumInactivityPeriod, scheduler))
+            select delta;
+        IObservable<int> outstanding = onoffs.Scan(0, (total, delta) => total + delta);
+        IObservable<int> zeroCrossings = outstanding.Where(total => total == 0);
+        return src.Buffer(zeroCrossings);
+    }
+}
+
---
+++ code/scripts/playground/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reactive.Linq;
+
+IObservable<long> ticks = Observable.Timer(
+    dueTime: TimeSpan.Zero,
+    period:  TimeSpan.FromSeconds(1));
+ticks.Subscribe(
+    tick => Console.WriteLine($"Tick: {tick}"));
+Console.ReadLine();
+
+// IObservable<int> numbers = Observable.Range(1, 100);
+// var windows = numbers.Buffer(5);
+// var averages = windows.Select(x => x.Average());
+// averages.Subscribe(x => Console.WriteLine(x));
+
